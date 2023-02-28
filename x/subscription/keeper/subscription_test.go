6da64207dd7fdca47b0ed8c82779f8d14cebbdb8
Oren Laadan <orenl@lavanet.xyz>
2023-02-28 06:06:12 -0500

    CNS-169: add tests for CreateSubscription, EpochStart

--- x/subscription/keeper/subscription_test.go
+++ x/subscription/keeper/subscription_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lavanet/lava/relayer/sigs"
 	keepertest "github.com/lavanet/lava/testutil/keeper"
 	"github.com/lavanet/lava/testutil/nullify"
+	epochstoragetypes "github.com/lavanet/lava/x/epochstorage/types"
 	"github.com/lavanet/lava/x/subscription/keeper"
 	"github.com/lavanet/lava/x/subscription/types"
 	"github.com/stretchr/testify/require"
@@ -66,4 +67,129 @@ func TestSubscriptionGetAll(t *testing.T) {
 		nullify.Fill(items),
 		nullify.Fill(keeper.GetAllSubscription(ctx)),
 	)
+}
+
+func TestCreateSubscription(t *testing. T) {
+	_, keepers, _ctx := keepertest.InitAllKeepers(t)
+	ctx := sdk.UnwrapSDKContext(_ctx)
+
+	keeper := keepers.Subscription
+	bank := keepers.BankKeeper
+
+	creators := []struct {
+		address string
+		amount  int64
+	}{
+		{
+			address: "FILL",
+			amount:  100000,
+		},
+		{
+			address: "FILL",
+			amount:  1,
+		},
+		{
+			address: "invalid creator",
+			amount:  0,
+		},
+	}
+
+	for i := range creators {
+		if creators[i].address == "FILL" {
+			_, addr := sigs.GenerateFloatingKey()
+			creators[i].address = addr.String()
+			coins := sdk.NewCoins(sdk.NewCoin(
+				epochstoragetypes.TokenDenom,
+				sdk.NewInt(creators[i].amount),
+			))
+			bank.SetBalance(ctx, addr, coins)
+		}
+	}
+
+	consumers := make([]string, 4)
+	for i := range consumers {
+		_, addr := sigs.GenerateFloatingKey()
+		consumers[i] = addr.String()
+	}
+	consumers[3] = "invalid consumer"
+
+	template := []struct {
+		name      string
+		index     string
+		creator   int
+		consumers []int
+		success   bool
+	}{
+		{
+			name:      "create subscriptions",
+			index:     "testplan",
+			creator:   0,
+			consumers: []int{0, 1},
+			success:   true,
+		},
+		{
+			name:      "invalid creator",
+			index:     "testplan",
+			creator:   2,
+			consumers: []int{2},
+			success:   false,
+		},
+		{
+			name:      "invalid consumer",
+			index:     "testplan",
+			creator:   0,
+			consumers: []int{3},
+			success:   false,
+		},
+		{
+			name:      "insufficient funds",
+			index:     "testplan",
+			creator:   1,
+			consumers: []int{2},
+			success:   false,
+		},
+		{
+			name:      "invalid plan",
+			index:     "",
+			creator:   0,
+			consumers: []int{2},
+			success:   false,
+		},
+		{
+			name:      "unknown plan",
+			index:     "no-such-plan",
+			creator:   0,
+			consumers: []int{2},
+			success:   false,
+		},
+		{
+			name:      "double subscription",
+			index:     "testplan",
+			creator:   0,
+			consumers: []int{0},
+			success:   false,
+		},
+	}
+
+	for _, tt := range template {
+		for _, consumer := range tt.consumers {
+			t.Run(tt.name, func(t *testing.T) {
+				sub := types.Subscription{
+					Creator:   creators[tt.creator].address,
+					Consumer:  consumers[consumer],
+					PlanIndex: tt.index,
+				}
+
+				err := keeper.CreateSubscription(
+					ctx, sub.Creator, sub.Consumer, sub.PlanIndex, sub.IsYearly)
+				if tt.success {
+					require.Nil(t, err, tt.name)
+					_, found := keeper.GetSubscription(ctx, sub.Consumer)
+					require.True(t, found, tt.name)
+				} else {
+					require.NotNil(t, err, tt.name)
+				}
+			})
+		}
+	}
 }
